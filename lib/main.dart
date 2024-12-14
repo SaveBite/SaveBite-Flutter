004aed6822377aa1340813b376624105e63bde34
IbrahimHosny2002 <hosnyhema66@gmail.com>
2024-12-15 00:53:26 +0200

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SaveBite());
+}
+
+class SaveBite extends StatelessWidget {
+  const SaveBite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
