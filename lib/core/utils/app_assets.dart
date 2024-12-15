1e1b416963298c63e00912ace08a9d51732ca7f4
IbrahimHosny2002 <hosnyhema66@gmail.com>
2024-12-15 02:48:16 +0200

    add app fonts and images

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAuthLogo
+  /// assets/images/auth_logo.svg
+  static const String imagesAuthLogo = "assets/images/auth_logo.svg";
+
+  /// Assets for imagesDotIcon
+  /// assets/images/dot_icon.svg
+  static const String imagesDotIcon = "assets/images/dot_icon.svg";
+
+  /// Assets for imagesDownIcon
+  /// assets/images/down_icon.svg
+  static const String imagesDownIcon = "assets/images/down_icon.svg";
+
+  /// Assets for imagesEyeOff
+  /// assets/images/eye-off.svg
+  static const String imagesEyeOff = "assets/images/eye-off.svg";
+
+  /// Assets for imagesForwardLeftIcon
+  /// assets/images/forward_left_icon.svg
+  static const String imagesForwardLeftIcon = "assets/images/forward_left_icon.svg";
+
+  /// Assets for imagesForwardRightIcon
+  /// assets/images/forward_right_icon.svg
+  static const String imagesForwardRightIcon = "assets/images/forward_right_icon.svg";
+
+  /// Assets for imagesLeftIcon
+  /// assets/images/left_icon.svg
+  static const String imagesLeftIcon = "assets/images/left_icon.svg";
+
+  /// Assets for imagesSplashLogo
+  /// assets/images/splash_logo.svg
+  static const String imagesSplashLogo = "assets/images/splash_logo.svg";
+
+  /// Assets for imagesUploadIcon
+  /// assets/images/upload_icon.svg
+  static const String imagesUploadIcon = "assets/images/upload_icon.svg";
+
+  /// Assets for imagesVerifiedIcon
+  /// assets/images/verified_icon.svg
+  static const String imagesVerifiedIcon = "assets/images/verified_icon.svg";
+}
+
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle styleRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff999999),
+  );
+  static const TextStyle styleRegular13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff5EDA42),
+  );
+  static const TextStyle styleMedium16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff333333),
+  );
+  static const TextStyle styleSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleMedium23 = TextStyle(
+    fontSize: 23,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff222222),
+  );
+  static const TextStyle styleRegular25 = TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff222222),
+  );
+  static const TextStyle styleRegular24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold10 = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleRegular28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff222222),
+  );
+  static const TextStyle styleRegular40 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff5EDA42),
+  );
+  static const TextStyle styleRegular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleRegular19 = TextStyle(
+    fontSize: 19,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff5EDA42),
+  );
+  static const TextStyle styleRegular32 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff222222),
+  );
+  static const TextStyle styleMedium28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff222222),
+  );
+  static const TextStyle styleMedium13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff333333),
+  );
+  static const TextStyle styleMedium19 = TextStyle(
+    fontSize: 19,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff5EDA42),
+  );
+  static const TextStyle styleRegular48 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleRegular33 = TextStyle(
+    fontSize: 33,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleRegular23 = TextStyle(
+    fontSize: 23,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleRegular11 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold48 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold40 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleMedium48 = TextStyle(
+    fontSize: 48,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleMedium40 = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold38 = TextStyle(
+    fontSize: 38,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold33 = TextStyle(
+    fontSize: 33,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold28 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold19 = TextStyle(
+    fontSize: 19,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold13 = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleBold11 = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+  static const TextStyle styleMedium33 = TextStyle(
+    fontSize: 33,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Noto Scans',
+    color: Color(0xff000000),
+  );
+}
